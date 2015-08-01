750a3990bee14ceba631a09eaaa73cce25891ca1
TsT <tst2005@gmail.com>
2015-08-01 21:37:02 +0200

    convert from sh to lua

--- make-all-in-one.sh
+++ make-all-in-one.sh
@@ -19,8 +19,51 @@ done
 
 #--mod 30log			thirdparty/git/yonaba/30log/30logclean.lua
 
-#"$ALLINONE" -e 'cmd_sheeban("featured.lua")'
-#exit 1
+lua -l "aio" -e '
+local aio = require "aio"
+local mod, rawmod, shebang, codehead = assert(aio.mod), assert(aio.rawmod), assert(aio.shebang), assert(aio.codehead)
+shebang(			"bin/featuredlua")
+codehead('"$headn"',		"bin/featuredlua")
+
+--$(if [ -n "$ICHECK" ]; then
+--        echo "--icheckinit"
+--fi)
+
+mod("preloaded",		"lib/preloaded.lua")
+mod("gro",			"thirdparty/git/tst2005/lua-gro/gro.lua")
+aio.require "gro"
+
+mod("strict",			"thirdparty/local/unknown/strict/strict.lua")
+aio.require	"strict"
+
+mod("i", 			"lib/i.lua")
+mod("generic",			"lib/generic.lua")
+
+mod("secs",			"thirdparty/local/bartbes/secs/secs.lua")
+mod("secs-featured",		"lib/secs-featured.lua")
+
+mod("middleclass",		"thirdparty/git/kikito/middleclass/middleclass.lua")
+mod("middleclass-featured",	"lib/middleclass-featured.lua")
+
+mod("30log",			"lib/30log-old.lua")
+mod("30log-featured",		"lib/30log-featured.lua")
+
+mod("compat_env",		"thirdparty/git/davidm/lua-compat-env/lua/compat_env.lua")
+
+mod("hump.class",		"thirdparty/git/vrld/hump/class.lua")
+
+mod("bit.numberlua",		"thirdparty/git/davidm/lua-bit-numberlua/lmod/bit/numberlua.lua")
+
+mod("lunajson",			"thirdparty/git/tst2005/lunajson/lunajson.lua")
+mod("utf8",			"thirdparty/git/tst2005/lua-utf8/utf8.lua")
+rawmod("cliargs",		"thirdparty/git/amireh/lua_cliargs/src/cliargs.lua")
+
+--$(if [ -n "$ICHECK" ]; then
+--	echo "--icheck"
+--fi)
+' > featured.lua
+
+exit
 
 "$ALLINONE" \
 --shebang			bin/featuredlua \
@@ -69,8 +112,6 @@ fi) \
 
 #--autoaliases
 #--code init.lua
-
-#--mod hump.class		thirdparty/git/vrld/hump/class.lua
 
 #"$ALLINONE" --shebang init.lua $( find hate/ -depth -name '*.lua' |while read -r line; do echo "--mod $(echo "$line" | sed 's,\.lua$,,g' | tr / .) ) --code init.lua
 
