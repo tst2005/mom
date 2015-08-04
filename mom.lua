3d39736f2860aa0b590e60e3418a11f30bf141a2
TsT <tst2005@gmail.com>
2015-08-04 23:35:21 +0200

    super-module is now named mom.lua

--- mom.lua
+++ mom.lua
@@ -13,8 +13,7 @@ _=[[
 	exit $?
 ]]
 _=nil
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("preloaded", ([[
--- <pack preloaded> --
+do local sources = {};sources["preloaded"]=([[-- <pack preloaded> --
 local _M = {}
 
 local preload = require "package".preload
@@ -47,9 +46,8 @@ _M.exists = exists
 _M.list = list
 
 return _M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("gro", ([[
--- <pack gro> --
+]]):gsub('\\([%]%[])','%1')
+sources["gro"]=([[-- <pack gro> --
 
 local _M = {}
 local loaded = require"package".loaded
@@ -116,10 +114,8 @@ local loaded = require"package".loaded
 --end
 
 return {}
-]]):gsub('\\([%]%[])','%1'))end
-require("gro")
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("strict", ([[
--- <pack strict> --
+]]):gsub('\\([%]%[])','%1')
+sources["strict"]=([[-- <pack strict> --
 --\[\[--
  Checks uses of undeclared global variables.
 
@@ -182,10 +178,8 @@ mt.__index = function (t, n)
   end
   return rawget (t, n)
 end
-]]):gsub('\\([%]%[])','%1'))end
-require("strict")
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("i", ([[
--- <pack i> --
+]]):gsub('\\([%]%[])','%1')
+sources["i"]=([[-- <pack i> --
 
 local _M = {}
 
@@ -320,9 +314,8 @@ end
 
 return _M
 
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("generic", ([[
--- <pack generic> --
+]]):gsub('\\([%]%[])','%1')
+sources["generic"]=([[-- <pack generic> --
 
 local mt = {}
 local _M = {}
@@ -344,9 +337,8 @@ end
 
 return setmetatable(_M, mt)
 
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("secs", ([[
--- <pack secs> --
+]]):gsub('\\([%]%[])','%1')
+sources["secs"]=([[-- <pack secs> --
 --\[\[
 Copyright (c) 2009-2011 Bart van Strien
 
@@ -391,9 +383,8 @@ function class:new(...)
 end
 
 return class
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("secs-featured", ([[
--- <pack secs-featured> --
+]]):gsub('\\([%]%[])','%1')
+sources["secs-featured"]=([[-- <pack secs-featured> --
 local secs = require "secs"
 
 local common = {}
@@ -413,9 +404,8 @@ pcall(function() require("i"):register("secs", common) end)
 local _M = {class = assert(common.class), instance = assert(common.instance), __BY = assert(common.__BY)}
 --_M.common = common
 return _M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("middleclass", ([[
--- <pack middleclass> --
+]]):gsub('\\([%]%[])','%1')
+sources["middleclass"]=([[-- <pack middleclass> --
 local middleclass = {
   _VERSION     = 'middleclass v3.0.1',
   _DESCRIPTION = 'Object Orientation for Lua',
@@ -598,9 +588,8 @@ middleclass.Object = Object
 setmetatable(middleclass, { __call = function(_, ...) return middleclass.class(...) end })
 
 return middleclass
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("middleclass-featured", ([[
--- <pack middleclass-featured> --
+]]):gsub('\\([%]%[])','%1')
+sources["middleclass-featured"]=([[-- <pack middleclass-featured> --
 local middleclass = require "middleclass"
 middleclass._LICENSE = "MIT"
 
@@ -638,9 +627,8 @@ pcall(function() require("classcommons2"):register("middleclass", common) end)
 local _M = {class = assert(common.class), instance = assert(common.instance), __BY = assert(common.__BY)}
 --_M.common = common
 return _M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("30log", ([[
--- <pack 30log> --
+]]):gsub('\\([%]%[])','%1')
+sources["30log"]=([[-- <pack 30log> --
 local assert, pairs, type, tostring, setmetatable = assert, pairs, type, tostring, setmetatable
 local baseMt, _instances, _classes, class = {}, setmetatable({},{__mode='k'}), setmetatable({},{__mode='k'})
 local function deep_copy(t, dest, aType)
@@ -671,9 +659,8 @@ local class = function(attr)
   c.is = function(self, kind) local super; while true do super = getmetatable(super or self) ; if super == kind or super == nil then break end ; end;
   return kind and (super == kind) end; return setmetatable(c,baseMt)
 end; return class
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("30log-featured", ([[
--- <pack 30log-featured> --
+]]):gsub('\\([%]%[])','%1')
+sources["30log-featured"]=([[-- <pack 30log-featured> --
 local class = require "30log"
 
 local common = {}
@@ -693,9 +680,8 @@ pcall(function() require("i"):register("30log", common) end)
 local _M = {class = assert(common.class), instance = assert(common.instance), __BY = assert(common.__BY)}
 --_M.common = common
 return _M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("compat_env", ([[
--- <pack compat_env> --
+]]):gsub('\\([%]%[])','%1')
+sources["compat_env"]=([[-- <pack compat_env> --
 --\[\[
   compat_env - see README for details.
   (c) 2012 David Manura.  Licensed under Lua 5.1/5.2 terms (MIT license).
@@ -811,7 +797,6 @@ else -- >= Lua 5.2
   end
   -- \[*\] might simulate with table keyed by coroutine.running()
   
---\[\[
   -- 5.1 style `setfenv` implemented in 5.2
   function M.setfenv(f, t)
     local f = envhelper(f, 'setfenv')
@@ -828,22 +813,6 @@ else -- >= Lua 5.2
     return f  -- invariant: original f ~= 0
   end
   -- \[*\] http://lua-users.org/lists/lua-l/2010-06/msg00313.html
-\]\]--
-
-    function M.setfenv(f, t)
-        f = (type(f) == 'function' and f or debug.getinfo(f + 1, 'f').func)
-        local name
-        local up = 0
-        repeat
-            up = up + 1
-            name = debug.getupvalue(f, up)
-        until name == '_ENV' or name == nil
-        if name then
-            debug.upvaluejoin(f, up, function() return name end, 1) -- use unique upvalue
-            debug.setupvalue(f, up, t)
-        end
-        if f ~= 0 then return f end
-    end
 
   -- 5.1 style `getfenv` implemented in 5.2
   function M.getfenv(f)
@@ -858,9 +827,8 @@ end
 
 
 return M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("hump.class", ([[
--- <pack hump.class> --
+]]):gsub('\\([%]%[])','%1')
+sources["hump.class"]=([[-- <pack hump.class> --
 --\[\[
 Copyright (c) 2010-2013 Matthias Richter
 
@@ -958,9 +926,8 @@ end
 -- the module
 return setmetatable({new = new, include = include, clone = clone},
 	{__call = function(_,...) return new(...) end})
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("hump.class-featured", ([[
--- <pack hump.class-featured> --
+]]):gsub('\\([%]%[])','%1')
+sources["hump.class-featured"]=([[-- <pack hump.class-featured> --
 --error("hump.class modify the global env ! FIXIT")
 
 -- interface for cross class-system compatibility (see https://github.com/bartbes/Class-Commons).
@@ -979,9 +946,8 @@ end
 common.__BY = "hump.class"
 pcall(function() require("i"):register("hump.class", common) end)
 return common
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("bit.numberlua", ([[
--- <pack bit.numberlua> --
+]]):gsub('\\([%]%[])','%1')
+sources["bit.numberlua"]=([[-- <pack bit.numberlua> --
 --\[\[
 
 LUA MODULE
@@ -1527,9 +1493,8 @@ function M.bit.bswap(x)
 end
 
 return M
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("lunajson", ([[
--- <pack lunajson> --
+]]):gsub('\\([%]%[])','%1')
+sources["lunajson"]=([[-- <pack lunajson> --
 require("package").preload\["lunajson._str_lib"\] = function(...)-- <pack lunajson._str_lib> --
 local inf = math.huge
 local byte, char, sub = string.byte, string.char, string.sub
@@ -2765,9 +2730,8 @@ return {
 	newparser = sax.newparser,
 	newfileparser = sax.newfileparser,
 }
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("utf8", ([[
--- <pack utf8> --
+]]):gsub('\\([%]%[])','%1')
+sources["utf8"]=([[-- <pack utf8> --
 local m = {} -- the module
 local ustring = {} -- table to index equivalent string.* functions
 
@@ -2976,9 +2940,8 @@ local mt = {
 }
 
 return setmetatable(m,mt)
-]]):gsub('\\([%]%[])','%1'))end
-do local loadstring=loadstring;(function(name, rawcode)require"package".preload[name]=function(...)return assert(loadstring(rawcode))(...)end;end)("cliargs", ([[
--- <pack cliargs> --
+]]):gsub('\\([%]%[])','%1')
+sources["cliargs"]=([[-- <pack cliargs> --
 local cli, _
 
 -- ------- --
@@ -3645,4 +3608,130 @@ if _TEST then
 end
 
 return cli
-]]):gsub('\\([%]%[])','%1'))end
+]]):gsub('\\([%]%[])','%1')
+sources["ser"]=([[-- <pack ser> --
+local pairs, ipairs, tostring, type, concat, dump, floor, format = pairs, ipairs, tostring, type, table.concat, string.dump, math.floor, string.format
+
+local function getchr(c)
+	return "\\" .. c:byte()
+end
+
+local function make_safe(text)
+	return ("%q"):format(text):gsub('\n', 'n'):gsub("\[\128-\255\]", getchr)
+end
+
+local oddvals = {\[tostring(1/0)\] = '1/0', \[tostring(-1/0)\] = '-1/0', \[tostring(0/0)\] = '0/0'}
+local function write(t, memo, rev_memo)
+	local ty = type(t)
+	if ty == 'number' then
+		t = format("%.17g", t)
+		return oddvals\[t\] or t
+	elseif ty == 'boolean' or ty == 'nil' then
+		return tostring(t)
+	elseif ty == 'string' then
+		return make_safe(t)
+	elseif ty == 'table' or ty == 'function' then
+		if not memo\[t\] then
+			local index = #rev_memo + 1
+			memo\[t\] = index
+			rev_memo\[index\] = t
+		end
+		return '_\[' .. memo\[t\] .. '\]'
+	else
+		error("Trying to serialize unsupported type " .. ty)
+	end
+end
+
+local kw = {\['and'\] = true, \['break'\] = true, \['do'\] = true, \['else'\] = true,
+	\['elseif'\] = true, \['end'\] = true, \['false'\] = true, \['for'\] = true,
+	\['function'\] = true, \['goto'\] = true, \['if'\] = true, \['in'\] = true,
+	\['local'\] = true, \['nil'\] = true, \['not'\] = true, \['or'\] = true,
+	\['repeat'\] = true, \['return'\] = true, \['then'\] = true, \['true'\] = true,
+	\['until'\] = true, \['while'\] = true}
+local function write_key_value_pair(k, v, memo, rev_memo, name)
+	if type(k) == 'string' and k:match '^\[_%a\]\[_%w\]*$' and not kw\[k\] then
+		return (name and name .. '.' or '') .. k ..'=' .. write(v, memo, rev_memo)
+	else
+		return (name or '') .. '\[' .. write(k, memo, rev_memo) .. '\]=' .. write(v, memo, rev_memo)
+	end
+end
+
+-- fun fact: this function is not perfect
+-- it has a few false positives sometimes
+-- but no false negatives, so that's good
+local function is_cyclic(memo, sub, super)
+	local m = memo\[sub\]
+	local p = memo\[super\]
+	return m and p and m < p
+end
+
+local function write_table_ex(t, memo, rev_memo, srefs, name)
+	if type(t) == 'function' then
+		return '_\[' .. name .. '\]=loadstring' .. make_safe(dump(t))
+	end
+	local m = {}
+	local mi = 1
+	for i = 1, #t do -- don't use ipairs here, we need the gaps
+		local v = t\[i\]
+		if v == t or is_cyclic(memo, v, t) then
+			srefs\[#srefs + 1\] = {name, i, v}
+			m\[mi\] = 'nil'
+			mi = mi + 1
+		else
+			m\[mi\] = write(v, memo, rev_memo)
+			mi = mi + 1
+		end
+	end
+	for k,v in pairs(t) do
+		if type(k) ~= 'number' or floor(k) ~= k or k < 1 or k > #t then
+			if v == t or k == t or is_cyclic(memo, v, t) or is_cyclic(memo, k, t) then
+				srefs\[#srefs + 1\] = {name, k, v}
+			else
+				m\[mi\] = write_key_value_pair(k, v, memo, rev_memo)
+				mi = mi + 1
+			end
+		end
+	end
+	return '_\[' .. name .. '\]={' .. concat(m, ',') .. '}'
+end
+
+return function(t)
+	local memo = {\[t\] = 0}
+	local rev_memo = {\[0\] = t}
+	local srefs = {}
+	local result = {}
+
+	-- phase 1: recursively descend the table structure
+	local n = 0
+	while rev_memo\[n\] do
+		result\[n + 1\] = write_table_ex(rev_memo\[n\], memo, rev_memo, srefs, n)
+		n = n + 1
+	end
+
+	-- phase 2: reverse order
+	for i = 1, n*.5 do
+		local j = n - i + 1
+		result\[i\], result\[j\] = result\[j\], result\[i\]
+	end
+
+	-- phase 3: add all the tricky cyclic stuff
+	for i, v in ipairs(srefs) do
+		n = n + 1
+		result\[n\] = write_key_value_pair(v\[2\], v\[3\], memo, rev_memo, '_\[' .. v\[1\] .. '\]')
+	end
+
+	-- phase 4: add something about returning the main table
+	if result\[n\]:sub(1, 5) == '_\[0\]=' then
+		result\[n\] = 'return ' .. result\[n\]:sub(6)
+	else
+		result\[n + 1\] = 'return _\[0\]'
+	end
+
+	-- phase 5: just concatenate everything
+	result = concat(result, '\n')
+	return n > 1 and 'local _={}\n' .. result or result
+end
+]]):gsub('\\([%]%[])','%1')
+local loadstring=loadstring; local preload = require"package".preload
+for name, rawcode in pairs(sources) do preload[name]=function(...)return loadstring(rawcode)(...)end end
+end;
